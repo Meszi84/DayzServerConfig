8f88821efdfffeef1a9aff628c5b9c8a9b69840c
Meszi84 <60156956+Meszi84@users.noreply.github.com>
2023-05-03 21:54:45 +0200

    Create Fishingv2_config.cpp

---
+++ Fishingv2_config.cpp
@@ -0,0 +1,12 @@
+class CfgVehicles
+{
+    class Fishing_Boat_Base;
+    class Fishing_Boat: Fishing_Boat_Base
+    {
+        class Fishing
+        {
+            item = "FishingRod";
+            probCatch = 0.3; // 30% halfogási esély
+        };
+    };
+};
