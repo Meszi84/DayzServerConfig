9eff53c01f91efc2a27794cc6c49d90ed244cd1c
Meszi84 <60156956+Meszi84@users.noreply.github.com>
2023-05-04 14:23:45 +0200

    Create armband_config.cpp

---
+++ armband_config.cpp
@@ -0,0 +1,44 @@
+class CfgPatches
+{
+	class FinalDay_Armbrand
+	{
+		units[]=
+		{
+			"Predators_Armbrand",
+			"Hungarian_Armbrand"
+			
+		};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Characters"
+		};
+	};
+};
+class CfgVehicles
+{
+    class Armband_ColorBase;
+	class Predators_Armband: Armband_ColorBase
+	{
+		displayName="Predators Armband";
+		descriptionShort="Predators team ";
+		color="PredatorsArmband";
+		scope=2;
+		hiddenSelectionsTextures[]=
+		{
+			"FinalDay_Flags\data\PredatorsFlag.paa"
+		};
+	};
+	class Hungarian_Armband: Armband_ColorBase
+	{
+		displayName="Hungarian Armband";
+		descriptionShort="Hungarian team ";
+		color="HungarianArmband";
+		scope=2;
+		hiddenSelectionsTextures[]=
+		{
+			"FinalDay_Flags\data\Hun_Flag.paa"
+		};
+	};
+};
